9173e50ec08561b0053952510768488fdec9eed4
Christian Vogel <vogelchr@vogel.cx>
2014-07-19 15:02:34 +0200

    sha1: add compatibility stuff to compile it outside of git

--- sha1.c
+++ sha1.c
@@ -7,7 +7,22 @@
  */
 
 /* this is only to get definitions for memcpy(), ntohl() and htonl() */
-#include "../git-compat-util.h"
+#include <string.h>
+#include <arpa/inet.h>
+
+#define get_be32(p) ( \
+	(*((unsigned char *)(p) + 0) << 24) | \
+	(*((unsigned char *)(p) + 1) << 16) | \
+	(*((unsigned char *)(p) + 2) <<  8) | \
+	(*((unsigned char *)(p) + 3) <<  0) )
+
+#define put_be32(p, v)	do { \
+	unsigned int __v = (v); \
+	*((unsigned char *)(p) + 0) = __v >> 24; \
+	*((unsigned char *)(p) + 1) = __v >> 16; \
+	*((unsigned char *)(p) + 2) = __v >>  8; \
+	*((unsigned char *)(p) + 3) = __v >>  0; } while (0)
+
 
 #include "sha1.h"
 
